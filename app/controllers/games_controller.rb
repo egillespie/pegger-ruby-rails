4b2e9244124f45ca28c71e07ce9ef395822b4808
Erik Gillespie <erik.gillespie@gmail.com>
2016-03-23 10:21:07 -0400

    rails generate controller Games start get

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,7 @@
+class GamesController < ApplicationController
+  def start
+  end
+
+  def get
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'games/start'
+
+  get 'games/get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
