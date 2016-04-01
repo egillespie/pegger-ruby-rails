197baabda4fca5c93eed2228a02bc8f0918ecb81
Erik Gillespie <erik.gillespie@gmail.com>
2016-04-01 14:27:04 -0400

    Fixed indentation

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -1,13 +1,13 @@
 class JsonController < ApplicationController
   before_filter :parse_request
 
-  private
-    def parse_request
-      request_body = request.body.read
-      if request_body.blank?
-        @json = nil
-      else
-        @json = JSON.parse(request.body.read)
-      end
+private
+  def parse_request
+    request_body = request.body.read
+    if request_body.blank?
+      @json = nil
+    else
+      @json = JSON.parse(request.body.read)
     end
+  end
 end
